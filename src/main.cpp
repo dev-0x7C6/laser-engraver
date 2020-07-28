03ed0a27f352de04067c8e37d311088a56a8cc7e
Bartłomiej Burdukiewicz <bartlomiej.burdukiewicz@gmail.com>
2020-07-28 18:41:57 +0200

    core: force to use C.UTF-8 for numeric conversions (to_string).

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,19 @@
 #include <QApplication>
 
+#include <clocale>
+#include <iostream>
+
 #include "mainwindow.h"
 
 int main(int argc, char *argv[]) {
+	// use dot separator for gcode generation
+	std::setlocale(LC_NUMERIC, "C.UTF-8");
+
+	if (auto sample = std::to_string(1.2); sample.find('.') == std::string::npos) {
+		std::cerr << "C library implementation not supporting C.UTF-8" << std::endl;
+		return 1;
+	}
+
 	QApplication application(argc, argv);
 	MainWindow window;
 	window.showMaximized();
