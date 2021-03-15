1575d0dba96dcff1fe92c6ffc518cdb946428d55
Bartłomiej Burdukiewicz <bartlomiej.burdukiewicz@gmail.com>
2021-03-15 17:02:26 +0100

    core: set numerical locale after QApplication.

--- src/main.cpp
+++ src/main.cpp
@@ -5,16 +5,23 @@
 
 #include "mainwindow.h"
 
-int main(int argc, char *argv[]) {
-	// use dot separator for gcode generation
+auto set_numerical_locale() -> int {
 	std::setlocale(LC_NUMERIC, "C.UTF-8");
 
-	if (auto sample = std::to_string(1.2); sample.find('.') == std::string::npos) {
+	if (const auto sample = std::to_string(1.2); sample.find('.') == std::string::npos) {
 		std::cerr << "C library implementation not supporting C.UTF-8" << std::endl;
 		return 1;
 	}
 
+	return 0;
+}
+
+auto main(int argc, char *argv[]) -> int {
 	QApplication application(argc, argv);
+
+	if (const auto ret = set_numerical_locale(); ret != 0)
+		return ret;
+
 	MainWindow window;
 	window.showMaximized();
 
